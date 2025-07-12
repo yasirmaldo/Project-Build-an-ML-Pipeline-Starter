b07e7427a02c060c997356642d20c36061ab1529
Yasir <yasirmaldo26@gmail.com>
2025-07-12 08:20:58 -0400

    got the ml training step to work

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -74,6 +74,8 @@ def go(args):
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
     # YOUR CODE HERE
+    sk_pipe.fit(X_train, y_train)
+
     ######################################
 
     # Compute r2 and MAE
@@ -96,7 +98,8 @@ def go(args):
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
     mlflow.sklearn.save_model(
-        # YOUR CODE HERE
+        sk_model=sk_pipe,
+        path="random_forest_dir",
         input_example = X_train.iloc[:5]
     )
     ######################################
@@ -120,6 +123,7 @@ def go(args):
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
     # YOUR CODE HERE
+    run.summary["mae"] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -162,6 +166,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder()
         # YOUR CODE HERE
     )
     ######################################
@@ -225,7 +231,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
 
     sk_pipe = Pipeline(
         steps =[
-        # YOUR CODE HERE
+            ("preprocessor", preprocessor),
+            ("random_forest", random_forest)
         ]
     )
 
