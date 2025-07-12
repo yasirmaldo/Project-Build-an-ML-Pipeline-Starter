2475fec3c3941afbefa873b7edfd7d11c39d5654
Yasir <yasirmaldo26@gmail.com>
2025-07-12 09:23:02 -0400

    Completed the pipeline and tested best model, time to release V1

--- main.py
+++ main.py
@@ -117,13 +117,15 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                env_manager="conda",
+                parameters={"mlflow_model": config["modeling"]["mlflow_model"],
+                            "test_dataset": config["modeling"]["test_dataset"]
+                    
+                }
+            )
 
 if __name__ == "__main__":
     go()
